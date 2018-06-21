a22b03153e56aa34a927ff8cc8d7466761a44a38
Emil Terman <terman.emil@gmail.com>
2018-06-21 17:11:54 +0300

    initialized the project

---
+++ RNN-LogicGate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RNN_LogicGate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
